3faa3fa04ea38095a467ee2f0b8110bb5d3496f0
Erick Junior <erickjuniorbatistagarcia2@gmail.com>
2024-05-14 09:18:02 -0300

    comitando as aulas

---
+++ src/academy/devdojo/maratonajava/introducao/Aula01OlaMundo.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+/**
+ *
+ * Isso é um comentario <p><b>javadoc</b></p>
+ * */
+public class Aula01OlaMundo {
+    // Isso é um comentario em linha utilizar somente quando o codigo é dificil de entender
+    /*
+     Isso é um comentario de multiplas linhas
+    */
+    public static void main(String[] args){
+        System.out.println("Olá Mundo!");
+    }
+}
