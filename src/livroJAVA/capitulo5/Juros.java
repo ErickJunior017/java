70ee135564b74fec599faeffadbad450817ffed5
Erick Junior <erickjuniorbatistagarcia2@gmail.com>
2024-07-08 03:21:50 -0300

    Terminei meus estudos com a estrutura for

--- src/livroJAVA/capitulo5/Juros.java
+++ src/livroJAVA/capitulo5/Juros.java
@@ -9,7 +9,7 @@ public class Juros {
         System.out.printf("%s%20s %n", "Year", "Amount on deposit");
         for (int year = 1; year <= 10; ++year){
             amount = principal * (Math.pow(rate, year));
-            System.out.printf("%4d%,20.2f%n", year, amount);
+            System.out.printf("%4d%20f%n", year, amount);
         }
     }
 }
