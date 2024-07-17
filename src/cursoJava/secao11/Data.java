e48dcf1600a2b0dc69b624d123a19c2d630e2750
Erick Junior <erickjuniorbatistagarcia2@gmail.com>
2024-07-16 22:11:31 -0300

    Data-Hora formato ISO 8601

---
+++ src/cursoJava/secao11/Data.java
@@ -0,0 +1,40 @@
+package cursoJava.secao11;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Data {
+    public static void main(String[] args) {
+        //https://docs.oracle.com/en/java/javase/22/docs/api/java.base/java/time/format/DateTimeFormatter.html
+
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        LocalDate d01 = LocalDate.now();
+        LocalDateTime d02 = LocalDateTime.now();
+        Instant d03 = Instant.now();
+        LocalDate d04 = LocalDate.parse("2024-07-16");
+        LocalDateTime d05 = LocalDateTime.parse("2024-07-16T21:29:26");
+        Instant d06 = Instant.parse("2024-07-16T21:29:26Z");
+        Instant d07 = Instant.parse("2024-07-16T21:29:26-03:00");
+        LocalDate d08 = LocalDate.parse("20/07/2022", fmt1);
+        LocalDateTime d09 = LocalDateTime.parse("20/07/2022 01:30", fmt2);
+
+        LocalDate d10 = LocalDate.of(2024, 07, 16);
+        LocalDateTime d11 = LocalDateTime.of(2024, 07, 16, 22, 9);
+
+        System.out.println(d01);
+        System.out.println(d02);
+        System.out.println(d03);
+        System.out.println(d04);
+        System.out.println(d05);
+        System.out.println(d06);
+        System.out.println(d07);
+        System.out.println(d08);
+        System.out.println(d09);
+        System.out.println(d10);
+        System.out.println(d11);
+    }
+}
+
