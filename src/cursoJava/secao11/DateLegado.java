024afee5f3adf02eb14183bd210823ad7f3266a9
Erick Junior <erickjuniorbatistagarcia2@gmail.com>
2024-07-19 00:22:06 -0300

    Classe Data e SimpleDateFormat

--- src/cursoJava/secao11/DateLegado.java
+++ src/cursoJava/secao11/DateLegado.java
@@ -4,6 +4,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class DateLegado {
     public static void main(String[] args) throws ParseException {
@@ -11,12 +12,50 @@ public class DateLegado {
         //Exemplo: 2024-07-17T00:49:56Z
         SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
         SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        SimpleDateFormat sdf3 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        sdf3.setTimeZone(TimeZone.getTimeZone("GMT"));
 
         Date y1 = sdf1.parse("25/06/2024");
         Date y2 = sdf2.parse("25/06/2024 15:27:07");
+        Date x1 = new Date();
+        Date x2 = new Date(System.currentTimeMillis());
+        Date x3 = new Date(0L);
+        Date x4 = new Date(1000L * 60L * 60L * 5L);
+        Date x5 = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
 
         System.out.println("y1: " + sdf1.parse("25/06/2024"));
         System.out.println("y2: " + sdf2.format(y1));
         System.out.println("y2: " + sdf2.format(y2));
+
+        SimpleDateFormat y8 = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        Date agora = y8.parse("18/07/2024 23:35");
+        System.out.println("agora = " + agora);
+        System.out.println("x1: " + x1);
+        System.out.println("x2: " + x2);
+        System.out.println("x3: " + x3);
+        System.out.println("x4: " + x4);
+        System.out.println("x4: " + x5);
+
+        System.out.println("------------------------");
+        System.out.println("y1: " + sdf1.parse("25/06/2024"));
+        System.out.println("y2: " + sdf2.format(y1));
+        System.out.println("y2: " + sdf2.format(y2));
+        System.out.println("agora = " + y8.format(agora));
+        System.out.println("x1: " + y8.format(x1));
+        System.out.println("x2: " + y8.format(x2));
+        System.out.println("x3: " + y8.format(x3));
+        System.out.println("x4: " + y8.format(x4));
+        System.out.println("x4: " + y8.format(x5));
+        System.out.println("------------------------");
+        System.out.println("y1: " + sdf1.parse("25/06/2024"));
+        System.out.println("y2: " + sdf3.format(y1));
+        System.out.println("y2: " + sdf3.format(y2));
+
+        System.out.println("agora = " + sdf3.format(agora));
+        System.out.println("x1: " + sdf3.format(x1));
+        System.out.println("x2: " + sdf3.format(x2));
+        System.out.println("x3: " + sdf3.format(x3));
+        System.out.println("x4: " + sdf3.format(x4));
+        System.out.println("x4: " + sdf3.format(x5));
     }
 }
