803b24a26f1264e884c0c67ed8409480b988477f
Erick Junior <erickjuniorbatistagarcia2@gmail.com>
2024-07-27 22:02:44 -0300

    pilhas de chamada de método

--- src/cursoJava/secao15/Program.java
+++ src/cursoJava/secao15/Program.java
@@ -5,19 +5,25 @@ import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+        method1();
+        System.out.println("End of program");
+    }
+
+    public static void method1(){
+        System.out.println("***METHOD1 START***");
+        method2();
+        System.out.println("***METHOD1 END***");
+    }
+
+    public static void method2(){
+        System.out.println("***METHOD2 START***");
         Scanner input = new Scanner(System.in);
 
-        try{
-            String[] vect = input.nextLine().split(" ");
-            int position = input.nextInt();
-            System.out.println(vect[position]);
-        }catch(ArrayIndexOutOfBoundsException e){
-            System.out.println("Invalid position!");
-        }catch(InputMismatchException a){
-            System.out.println("Input error!");
-        }
+        String[] vect = input.nextLine().split(" ");
+        int position = input.nextInt();
+        System.out.println(vect[position]);
 
-        System.out.println("End of program");
+        System.out.println("***METHOD2 END***");
 
         input.close();
     }
