5c60871668d9128c491999f22c8959bcc02e5d26
Erick Junior <erickjuniorbatistagarcia2@gmail.com>
2024-08-16 00:46:24 -0300

    BufferedWriter and FileWriter

---
+++ src/cursoJava/secao17/Program3.java
@@ -0,0 +1,31 @@
+package cursoJava.secao17;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Program3 {
+    public static void main(String[] args) {
+
+        String[] lines = new String[]{
+                "Good morning",
+                "Good afternoon",
+                "Good night",
+                "Good evening"
+        };
+
+        String path = "C:\\temp\\out.txt";
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
+
+            for (String line : lines){
+                bw.write(line);
+                bw.newLine();
+            }
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+}
